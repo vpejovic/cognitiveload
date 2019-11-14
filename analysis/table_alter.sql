33518138b0821f75de50dfb3d20d59689b5a4692
Veljko Pejovic <veljko@Veljko.fri1.uni-lj.si>
2019-11-14 16:55:56 +0100

    Decomposing TLX into sub-measures

---
+++ analysis/table_alter.sql
@@ -0,0 +1,7 @@
+ALTER TABLE primary_task
+	ADD COLUMN mental int, 
+	ADD COLUMN physical int, 
+	ADD COLUMN temporal int, 
+	ADD COLUMN performance int, 
+	ADD COLUMN effort int, 
+	ADD COLUMN frustration int;
